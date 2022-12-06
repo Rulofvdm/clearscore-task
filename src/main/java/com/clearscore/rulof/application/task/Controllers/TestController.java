25f88490e6c16206f24f367c264ac74cdbccdfa0
Rulof <rulof@gmail.com>
2022-12-06 07:11:22 +0200

    Got hello world up and running.

---
+++ src/main/java/com/clearscore/rulof/application/task/Controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.clearscore.rulof.application.task.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String GetTest(){
+        return "Hello wurld!";
+    }
+}
