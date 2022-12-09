b748067abe624cbeec30663e07d6647fed7cf6b1
Rulof van der Merwe <rulof@ergopak.co.za>
2022-12-09 16:09:56 +0200

    Made the server port read from an environmental variable

--- src/main/java/com/clearscore/rulof/application/task/Application.java
+++ src/main/java/com/clearscore/rulof/application/task/Application.java
@@ -4,10 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static boolean isParsable(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (final NumberFormatException e) {
+			return false;
+		}
+	}
+
+	private static void setPort() throws IOException {
+		String port = System.getenv("HTTP_PORT");
+		if (isParsable(port)){
+			System.setProperty("server.port", port);
+		}
+		else{
+			throw new IOException("Cant parse Port in HTTP_PORT environmental variable.");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		setPort();
 		SpringApplication.run(Application.class, args);
 	}
 }
